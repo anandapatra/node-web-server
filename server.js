6b3ec7e656d67334f0adbc17dee5afdda0bc1afc
Ananda Patra <ananda@Anandas-MBP.home>
2017-06-11 20:55:13 -0400

    Set up startup and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs'); // view engine for express
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials'); // registering pertials to handlebar template
@@ -45,6 +46,6 @@ app.get('/bad', (req, res) =>{
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
